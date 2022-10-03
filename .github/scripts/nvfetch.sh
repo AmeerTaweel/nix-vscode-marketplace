f214d065cc1050c68511ed8c5a0a5b2af4e68d2a
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-03 19:01:07 +0300

    try with profile

--- .github/scripts/nvfetch.sh
+++ .github/scripts/nvfetch.sh
@@ -7,15 +7,21 @@ file=nvfetch/$name.toml
 mkdir -p tmp
 tmp=tmp/$name.toml
 
-wc=$(nix build --print-out-paths --no-link nixpkgs#uwc)/bin/uwc
-nvfetcher=$(nix build --print-out-paths --no-link github:berberman/nvfetcher)/bin/nvfetcher
-awk=$(nix build --print-out-paths --no-link nixpkgs#gawk)/bin/awk
+# nix build
+# export wc=$(nix build --print-out-paths --no-link nixpkgs#uwc)/bin/uwc
+# echo $wc
+# export nvfetcher=$(nix build --print-out-paths --no-link github:berberman/nvfetcher)/bin/nvfetcher
+# echo $nvfetcher
+nix profile install nixpkgs#gawk nixpkgs#nvfetcher nixpkgs#wc
+nix profile install nixpkgs#nvfetcher
+nix profile install nixpkgs
+
+echo $(which awk)
+
+# export awk=$(nix build --print-out-paths --no-link nixpkgs#gawk)/bin/awk
+# echo $awk
 
 let "count = $(cat $file | awk '/\[/{++a}END{print a}') / 2 / $block_size + 1"
-
-echo $wc
-echo $nvfetcher
-echo $awk
 
 echo $BLOCK_SIZE
 echo $count
@@ -24,7 +30,7 @@ for i in $(seq $count);
 do
     let "block = i * $block_size * 2"
     printf "\n\n[ block: %s  ]\n\n" $i
-    cat nvfetch/$name.toml | gawk -v num=$block '/\[/ {++a} {if (a <= num) {print}}' > $tmp
+    cat nvfetch/$name.toml | awk -v num=$block '/\[/ {++a} {if (a <= num) {print}}' > $tmp
     # https://github.com/berberman/nvfetcher#cli
     nvfetcher -j 0 -o generated/$name -c $tmp -t
 done
