e95f496b8e4a90c5f82828cab095add5add38701
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-02 14:40:57 +0300

    fix add poetry

--- flake.nix
+++ flake.nix
@@ -59,24 +59,25 @@
           nvfetch = pkgs.writeShellApplication {
             name = "nvfetch";
             runtimeInputs = [ pkgs.poetry ];
-						text = ''
-							poetry install
-							dir="tmp/init"
-              init_json="$dir/init.json"
-              init_nix="$dir/init.nix"
-							poetry run python -m scripts.nvfetch \
-								--name "$NAME" --first-block "$FIRST_BLOCK" \
-								--block-size "$BLOCK_SIZE" --block-limit "$BLOCK_LIMIT" \
-								--init-json "''${INIT_JSON:-$init_json}" --init-nix "''${INIT_NIX:-$init_nix}" \
-                --threads "''${THREADS:-0}" --action-id "$ACTION_ID"
-						'';
+            text = ''
+              							poetry install
+              							dir="tmp/init"
+                            init_json="$dir/init.json"
+                            init_nix="$dir/init.nix"
+              							poetry run python -m scripts.nvfetch \
+              								--name "$NAME" --first-block "$FIRST_BLOCK" \
+              								--block-size "$BLOCK_SIZE" --block-limit "$BLOCK_LIMIT" \
+              								--init-json "''${INIT_JSON:-$init_json}" --init-nix "''${INIT_NIX:-$init_nix}" \
+                              --threads "''${THREADS:-0}" --action-id "$ACTION_ID"
+              						'';
           };
           combine = pkgs.writeShellApplication {
             name = "combine";
             text = ''
-							poetry run python -m scripts.combine \
-								--name "$NAME" --out-dir "''${OUT_DIR:-./}"
-						'';
+              							poetry run python -m scripts.combine \
+              								--name "$NAME" --out-dir "''${OUT_DIR:-./}"
+              						'';
+            runtimeInputs = [ pkgs.poetry ];
           };
         };
 
@@ -84,26 +85,26 @@
       {
         devShell = pkgs.mkShell {
           shellHook = ''
-                			export DENO_DIR="$(pwd)/.deno"
-            					export BLOCK_SIZE=1
-            					export BLOCK_LIMIT=1
-											export FIRST_BLOCK=1
-            					export NAME=vscode-marketplace
-                      export THREADS=0
-                      export OUT_DIR=tmp/out
-                      export ACTION_ID=1
-											# export INIT_JSON=generated/vscode-marketplace/generated.json
-											# export INIT_NIX=generated/vscode-marketplace/generated.nix
-            			'';
+                            			export DENO_DIR="$(pwd)/.deno"
+                        					export BLOCK_SIZE=1
+                        					export BLOCK_LIMIT=1
+            											export FIRST_BLOCK=1
+                        					export NAME=vscode-marketplace
+                                  export THREADS=0
+                                  export OUT_DIR=tmp/out
+                                  export ACTION_ID=1
+            											# export INIT_JSON=generated/vscode-marketplace/generated.json
+            											# export INIT_NIX=generated/vscode-marketplace/generated.nix
+                        			'';
           nativeBuildInputs = with pkgs; [
             deno
             nvfetcher
-            gawk
-            poetry
+            # gawk
+            # poetry
           ];
           buildInputs = [ ];
         };
-        packages = extensions // {inherit scripts;};
+        packages = extensions // { inherit scripts; };
         overlays.default = final: prev: {
           vscode-marketplace = extensions;
         };
