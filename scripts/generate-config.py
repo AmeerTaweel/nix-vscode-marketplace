9d63872be5a7272bbd558252e8140214bb2e6e16
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-03 00:19:35 +0300

    refactor?

--- scripts/generate-config.py
+++ scripts/generate-config.py
@@ -4,29 +4,46 @@ from scripts.common import write_file
 from pathlib import Path
 
 
-TOTAL = 55000
+APPROX_TOTAL = 55000
 BLOCK_SIZE = 100
 BLOCK_LIMIT = 10
 TARGET = "vscode-marketplace"
-CONFIG_TARGET = "vscode"
-approx_blocks = int(TOTAL / (BLOCK_SIZE * BLOCK_LIMIT))
+NAME = "VSCode Marketplace"
+ALLOW_NET = "marketplace.visualstudio.com"
+APPROX_BLOCKS = int(APPROX_TOTAL / (BLOCK_SIZE * BLOCK_LIMIT))
+
 include = [
     {"ACTION_ID": i + 1, "FIRST_BLOCK": i * BLOCK_LIMIT + 1}
-    for i in range(approx_blocks)
+    for i in range(APPROX_BLOCKS)
 ]
 NUMBER_JOBS = len(include) + 1
 
-t = {
-    "ALLOW_NET": ["marketplace.visualstudio.com"],
-    "NAME": ["VSCode Marketplace"],
+nvfetch = {
+    "ALLOW_NET": [ALLOW_NET],
+    "NAME": [NAME],
     "TARGET": [TARGET],
     "BLOCK_SIZE": [BLOCK_SIZE],
     "BLOCK_LIMIT": [BLOCK_LIMIT],
-    "NUMBER_JOBS": [NUMBER_JOBS],
     "include": include,
 }
 
+configs_dir = Path(".github") / "workflows" / "configs"
 write_file(
-    Path(".github") / "workflows" / "configs" / f"nvfetch-{TARGET}.json",
-    str(json.dumps(t, indent=2)),
+    configs_dir / f"nvfetch-{TARGET}.json",
+    str(json.dumps(nvfetch, indent=2)),
+)
+
+combine = {
+    "include": [
+        {
+            "TARGET": [TARGET],
+            "NAME": [NAME],
+            "NUMBER_JOBS": [NUMBER_JOBS],
+        }
+    ]
+}
+
+write_file(
+    configs_dir / f"combine-{TARGET}.json",
+    str(json.dumps(combine, indent=2)),
 )
