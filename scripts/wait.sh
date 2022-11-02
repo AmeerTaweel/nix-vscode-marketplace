ef46a0ff8925c2e32d3d45e84ec40bc9435fd039
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-03 01:23:48 +0300

    add order pushes

---
+++ scripts/wait.sh
@@ -0,0 +1,15 @@
+ACTION_ID=${ACTION_ID:-1}
+
+let "PREV_ID = $ACTION_ID - 1"
+PREV_GENERATED_ID=$([[ $ACTION_ID = 1 ]] && echo 1 || echo $PREV_ID)
+
+PREV_GENERATED="blocks/generated/vscode-marketplace/generated-$PREV_GENERATED_ID.json"
+
+WAIT_SECONDS=3
+
+while [[ ! -f $PREV_GENERATED ]]
+do
+    git pull || echo "waiting $WAIT_SECONDS seconds" && sleep $WAIT_SECONDS
+done
+
+echo "ready to push"
